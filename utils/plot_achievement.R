9443700a54d00841f1d62bbcbee70f4feda9ff1b
Nami Sunami <naoyuki.sunami@gmail.com>
2022-05-31 16:25:32 -0400

    Rename `plot_education` to `plot_achievement`

--- utils/plot_achievement.R
+++ utils/plot_achievement.R
@@ -1,21 +1,24 @@
 library(here)
 
-combined_data <- read_rds(here("data/processed/education.rds"))
+# Load data
+education_data <- read_rds(here("data/processed/education_achievement.rds"))
 
 # Set colors for the data 
-orgs <- unique(combined_data$organization)
+orgs <- unique(education_data$organization)
 orgs_color <- RColorBrewer::brewer.pal(length(orgs), "Set2")
 names(orgs_color) <- orgs
-combined_data <- combined_data %>%
+education_data <- education_data %>%
   mutate(plot_color = recode(organization, !!!orgs_color))
 
 
 # Function to plot the achievement over time
-plot_education <- function(selected_contentarea = c("ELA", "MATH"),
-                           selected_orgs = c("State of Delaware")){
-  cur_data <- combined_data %>%
+plot_achievement <- function(selected_contentarea = c("ELA", "MATH"),
+                           selected_orgs = c("State of Delaware"),
+                           selected_grades = "All Students"){
+  cur_data <- education_data %>%
     filter(contentarea %in% selected_contentarea) %>%
-    filter(organization %in% selected_orgs) 
+    filter(organization %in% selected_orgs) %>% 
+    filter(subgroup %in% selected_grades)
   
   cur_plot <- cur_data %>% 
     ggplot(aes(x = schoolyear, y = pctproficient,
@@ -37,5 +40,5 @@ plot_education <- function(selected_contentarea = c("ELA", "MATH"),
 }
 
 # Choices for UI
-contentareas <- unique(combined_data$contentarea)
-groups <- unique(combined_data$organization)
+contentareas <- unique(education_data$contentarea)
+groups <- unique(education_data$organization)

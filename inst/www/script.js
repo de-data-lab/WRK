0c438d0ab571a774703898ed9038c107caa28019
Nami Sunami <naoyuki.sunami@gmail.com>
2022-07-14 14:22:45 -0400

    Prevent the logo from wrapping via JS

---
+++ inst/www/script.js
@@ -0,0 +1,3 @@
+// Add `d-flex` class to the container for the title
+// to prevent the logo and the title wrapping into separate lines
+document.querySelector(".navbar-header").classList.add("d-flex")
